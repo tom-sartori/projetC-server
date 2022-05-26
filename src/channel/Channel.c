e70fad59056939b01b7d9f729d0a780bed760d40
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2022-05-26 14:59:19 +0200

    Size of channels.

--- src/channel/Channel.c
+++ src/channel/Channel.c
@@ -9,14 +9,14 @@
  * @param isPublic
  * @return
  */
-Channel *createChannel (char *name, int port, int isPublic) {
+Channel *createChannel (char *name, int port, int isPublic, int size) {
     Channel *channel = (Channel *)malloc(sizeof(Channel));
 
     channel->name = name;   /// TODO : Check unique name.
     channel->port = port;
     channel->serverSocketDescriptor = launchServer(port);
     channel->isPublic = isPublic;
-    channel->size = DEFAULT_CHANNEL_SIZE;
+    channel->size = size;
     channel->clientList = createList();
 
     return channel;
@@ -24,12 +24,12 @@ Channel *createChannel (char *name, int port, int isPublic) {
 
 
 void initChannelList () {
-    channelList[0] = createChannel(CHANNEL_FILE_NAME, CHANNEL_FILE_PORT, 0);
-    channelList[1] = createChannel(CHANNEL_SWITCH_NAME, CHANNEL_SWITCH_PORT, 0);
-    channelList[2] = createChannel(CHANNEL_DEFAULT_NAME, CHANNEL_DEFAULT_PORT, 0);
-    channelList[3] = createChannel(CHANNEL_1_NAME, CHANNEL_1_PORT, 1);
-    channelList[4] = createChannel(CHANNEL_2_NAME, CHANNEL_2_PORT, 1);
-    channelList[5] = createChannel(CHANNEL_3_NAME, CHANNEL_3_PORT, 1);
+    channelList[0] = createChannel(CHANNEL_FILE_NAME, CHANNEL_FILE_PORT, 0, NB_MAX_CLIENT);
+    channelList[1] = createChannel(CHANNEL_SWITCH_NAME, CHANNEL_SWITCH_PORT, 0, NB_MAX_CLIENT);
+    channelList[2] = createChannel(CHANNEL_DEFAULT_NAME, CHANNEL_DEFAULT_PORT, 0, NB_MAX_CLIENT);
+    channelList[3] = createChannel(CHANNEL_1_NAME, CHANNEL_1_PORT, 1, NB_MAX_CLIENT);
+    channelList[4] = createChannel(CHANNEL_2_NAME, CHANNEL_2_PORT, 1, DEFAULT_CHANNEL_SIZE);
+    channelList[5] = createChannel(CHANNEL_3_NAME, CHANNEL_3_PORT, 1, DEFAULT_CHANNEL_SIZE);
     // Don't forget to update NB_CHANNEL and create at the new index.
 }
 
